f066ddb9334c7e38f083a7bf600975846f659937
Hans Ospina <hansospina@gmail.com>
2020-01-20 17:02:30 -0500

    fixed release script

--- release.sh
+++ release.sh
@@ -7,25 +7,9 @@ if [[ -z "$VERSION" ]]; then
   echo "ERROR: Version is undefined"
   exit 1
 fi
-#if [[ -z "${SONATYPE_USERNAME}" ]]; then
-#  echo "ERROR: SONATYPE_USERNAME is undefined"
-#  exit 1
-#fi
-#if [[ -z "${SONATYPE_PASSWORD}" ]]; then
-#  echo "ERROR: SONATYPE_PASSWORD is undefined"
-#  exit 1
-#fi
-#if [[ -z "${GPG_KEYNAME}" ]]; then
-#  echo "ERROR: GPG_KEYNAME is undefined"
-#  exit 1
-#fi
-#if [[ -z "${GPG_PASSPHRASE}" ]]; then
-#  echo "ERROR: GPG_PASSPHRASE is undefined"
-#  exit 1
-#fi
 
 mvn versions:set -DnewVersion=${VERSION} -DgenerateBackupPoms=false
 mvn clean deploy -B -P release --settings ${DIR}/settings.xml
 git checkout .
 git tag $VERSION
-git push tags
+git push upstream --tags
